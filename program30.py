ed67727648ebc89feca3fbe59be63e96ada9a9f0
Nandhups <nandhanaps200@gmail.com>
2025-01-10 11:11:29 +0530

    Add files via upload

--- program30.py
+++ program30.py
@@ -3,5 +3,5 @@ r_area=lambda length,breadth:length*breadth
 s_area=lambda side:side**2
 print("area of triangle (10,5) is:",t_area(10,5))
 print("area of rectangle (30,20) is:",r_area(30,20))
-print("area of square(5) is:",s_area(5))
+print("area o
 
